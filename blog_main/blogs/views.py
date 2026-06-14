8386b65e0534d030d205fd34d7748c0f4a1e4b7b
jatin-agrawal17 <mittaljatin441@gmail.com>
2026-06-14 23:11:44 +0530

    featured and simple posts

--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blogs.models import Blog
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status = 'Published', category_id=category_id)
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts_by_category.html', context)
